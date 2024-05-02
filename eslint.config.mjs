f872ab732631691b8c4f46e5d06fac9fb0c4132e
Frank Scott <33237633+Ziorks@users.noreply.github.com>
2024-05-02 09:24:55 -0500

    Added ignores to eslint.config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,17 @@ import eslintConfigPrettier from "eslint-config-prettier";
 export default [
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
   { languageOptions: { globals: globals.browser } },
+  {
+    ignores: [
+      "node_modules/*",
+      "dist/*",
+      "packaje.json",
+      "package-lock.json",
+      "webpack.common.js",
+      "webpack.dev.js",
+      "webpack.prod.js",
+    ],
+  },
   pluginJs.configs.recommended,
   eslintConfigPrettier,
 ];

5d94fe0ddd2edab45d3aa79bcf6e14cad9592cde
Frank Scott <33237633+Ziorks@users.noreply.github.com>
2024-05-07 13:23:20 -0500

    Added babel to webpack config

--- webpack.common.js
+++ webpack.common.js
@@ -11,6 +11,17 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            cacheDirectory: true,
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+      {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },

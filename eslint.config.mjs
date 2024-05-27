c94e711fa2f426500268e7178c555f6a24a05e89
Frank Scott <33237633+Ziorks@users.noreply.github.com>
2024-05-27 05:23:29 -0500

    Added jest linting for src/test folder

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,36 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
+import jest from "eslint-plugin-jest";
 import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
   { files: ["**/*.js"], languageOptions: { sourceType: "module" } },
+  {
+    files: ["src/test/**"],
+    ...jest.configs["flat/recommended"],
+    ...jest.configs["flat/style"],
+    rules: {
+      ...jest.configs["flat/recommended"].rules,
+      ...jest.configs["flat/style"].rules,
+      "jest/prefer-expect-assertions": "off",
+    },
+    languageOptions: { globals: jest.environments.globals.globals },
+  },
   { languageOptions: { globals: globals.browser } },
+  {
+    files: ["test/**"],
+    ...jest.configs["flat/style"],
+    rules: {
+      ...jest.configs["flat/style"].rules,
+      "jest/prefer-expect-assertions": "off",
+    },
+    languageOptions: { globals: globals.jest },
+  },
   {
     ignores: [
       "node_modules/*",
       "dist/*",
-      "packaje.json",
+      "package.json",
       "package-lock.json",
       "webpack.common.js",
       "webpack.dev.js",
